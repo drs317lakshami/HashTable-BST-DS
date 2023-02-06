f0525ee07471168d74fec0ff2c8d0f78143e1b6b
drs317lakshami <devrajkumar317@gmail.com>
2023-02-06 19:03:34 +0530

    Added : Removing of words from paragraph

--- src/com/bridgelabz/hastable/HashTable.java
+++ src/com/bridgelabz/hastable/HashTable.java
@@ -6,7 +6,10 @@ import java.util.Set;
 public class HashTable {
 
 	static void userInputSentence(String userString) {
-
+		/*
+		 * Declaring a HashMap Splitting the word of string and storing them in the
+		 * array.
+		 */
 		HashMap<String, Integer> frequency = new HashMap<String, Integer>();
 		String[] words = userString.split(" "); // splitting to find word
 		for (String word : words) {
@@ -27,8 +30,34 @@ public class HashTable {
 		}
 	}
 
+	// Remove method
+	public static void remove(String string, String removeWord) {
+		// split words from the sentence to store in String array using split()
+		String[] stringArray = string.split(" ");
+		String newSentenceAfterRemovingWord = " ";
+
+		// Iterating By using for each loop
+		for (String words : stringArray) {
+			// ! exclamation mark is used as not equal to sign
+			if (!words.equals(removeWord)) {
+				// iterate till end and remove "avoidable" from the paragraph
+				newSentenceAfterRemovingWord = newSentenceAfterRemovingWord + words + " ";
+
+			}
+		}
+		// new sentence after removing "avoidable" word.
+		System.out.println(newSentenceAfterRemovingWord);
+	}
+
 	public static void main(String[] args) {
 		userInputSentence("Paranoids are not paranoid because they are paranoid but because "
 				+ "they keep putting themselves deliberately into paranoid avoidable situations");
+
+		System.out.println("=====================================================");
+
+		String string = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		String removeWord = "avoidable";
+		remove(string, removeWord);
+
 	}
 }
