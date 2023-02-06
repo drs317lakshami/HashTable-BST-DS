f9385d2a3b500363092106b2a552828f00525065
drs317lakshami <devrajkumar317@gmail.com>
2023-02-06 19:33:12 +0530

    Added: recursively search 63 in the Binary Tree

--- src/com/bridgelabz/hastable/BinaryNode.java
+++ src/com/bridgelabz/hastable/BinaryNode.java
@@ -1,12 +1,9 @@
 package com.bridgelabz.hastable;
 
 public class BinaryNode<K extends Comparable<K>> {
-
     K key;
     BinaryNode<K> left;
     BinaryNode<K> right;
-
-
     public BinaryNode(K key) {
         this.key = key;
         this.left = null;
