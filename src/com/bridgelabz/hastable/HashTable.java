647e4623f027732d93c21ba4f0a996f005a0d0c0
drs317lakshami <devrajkumar317@gmail.com>
2023-02-06 18:50:08 +0530

    Added : To getting the frequency of words in sentence

--- src/com/bridgelabz/hastable/HashTable.java
+++ src/com/bridgelabz/hastable/HashTable.java
@@ -1,8 +1,29 @@
 package com.bridgelabz.hastable;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class HashTable {
 
+	static void InputSentence(String userString) {
+		HashMap<String, Integer> frequency = new HashMap<String, Integer>();
+		String[] words = userString.split(" ");
+		for (String word : words) {
+			if (frequency.containsKey(word)) {
+				frequency.put(word, frequency.get(word) + 1);
+			} else {
+				frequency.put(word, 1);
+			}
+		}
+		Set<String> stringFrequency = frequency.keySet();
+		System.out.println("Frequency Of Words In Sentence: \n 'To Be Or Not To Be'");
+		for (String word : stringFrequency) {
+			System.out.println(word + " = " + frequency.get(word) + " times.");
+
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to hashtable and binary serch tree");
+		InputSentence("To Be Or Not To Be");
 	}
 }
